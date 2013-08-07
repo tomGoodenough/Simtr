0bba16aba6522ac9148e44e98eef13554fb7d074
Tom Goodenough <tom-goodenough@hotmail.co.uk>
2013-08-07 13:12:34 +0100

    CSS Styling
    Gave the game a medieveal theme

--- index.php
+++ index.php
@@ -1,42 +1,52 @@
 <!DOCTYPE html>
 <html>
     <head>
+    	<link href="css/style.css" rel="stylesheet" type="text/css" />
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
     </head>
     <body>
+    
+    <div id="content">
         <form action="includes/login.php" method="POST">
             <hgroup>
-            <h1>Simtr</h1>
-            <h2>simple PBBG (Persistent Browser Based Game)</h2>
+            <! Added shadows here for the headings, to create an 'engraved' effect on the text - perhaps could work better with a stone type background? !>
+            <h1 style="font-size:50px;text-shadow: 0px 2px 3px #CCCCCC">Simtr</h1>
+            <h2 style="text-shadow: 0px 2px 3px #CCCCCC;">Simple PBBG (Persistent Browser Based Game)</h2>
             </hgroup>
-            <table>
+            <table align="center">
                 <tr>
                     <td>
                         <p>Login:</p>
                     </td>
                     <td>
-                        <input type="text" name="username" id="username"></input>                      
+                    <! Rounded text inputs made with border radius and gave fantasy theme to inputs + buttons with it !>
+                        <input style="border-radius:10px;" type="text" name="username" id="username"></input>                      
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p>Password:</p>
+                       Password:
                     </td>
                     <td>
-                        <input type="password" name="password" id="password"></input>
+                        <input style="border-radius:10px;"type="password" name="password" id="password"></input>
                     </td>
                 </tr>
                 <tr>
+               
                     <td>
-                        <button type="submit" id="submit">Login</button>
+                     <br/>
+                        <button style="font-family:fantasy;border-radius:10px;" type="submit" id="submit">Login</button>
                     </td>
                     <td>
-                        <button type="button" id="register" onclick="location.href='register.php';">Register</button>
+                    <br/>
+                        <button style="font-family:fantasy;border-radius:10px;" type="button" id="register" onclick="location.href='register.php';">Register</button>
                     </td>
                 </tr>  
             </table>     
         </form>
+        
+        </div>
         <?php
         
         ?>
